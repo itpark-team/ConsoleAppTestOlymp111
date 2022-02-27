677506daa415f020bee60cf4e747d4f26953b988
itpark-team <46758840+itpark-team@users.noreply.github.com>
2022-02-27 19:07:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppTestOlymp111/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    private static void Main()
+    {
+        StreamReader reader = new StreamReader("input.txt");
+        StreamWriter writer = new StreamWriter("output.txt");
+
+        string[] inputValues = reader.ReadLine().Split(' ');
+        int n = int.Parse(inputValues[0]);
+        int a = int.Parse(inputValues[1]);
+        int b = int.Parse(inputValues[2]);
+
+        int result = n * a * b * 2;
+
+        writer.WriteLine(result);
+
+        reader.Close();
+        writer.Close();
+    }
+}
